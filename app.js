9d07eb9be5cb7f76131491c4f858d4c758860c21
Saivald02 <Saivald02@gmail.com>
2018-12-13 01:07:53 +0000

    testing io

---
+++ app.js
@@ -0,0 +1,30 @@
+// https://github.com/amkurian/simple-chat
+
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server);
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get(
+      "https://api.darksky.net/forecast/e3b1e048dd74c7ca5d0a5263e2ca792d/37.8267,-122.4233"
+    );
+    socket.emit("FromAPI", res.data.currently.temperature);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+server.listen(port, () => console.log(`Listening on port ${port}`));
