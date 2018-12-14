98992b0c4953171b055b7a175a3cab84f19e12ae
Saivald02 <Saivald02@gmail.com>
2018-12-14 18:48:04 +0000

    test 77

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const path = require('path')
-//const app = express();
+const app = express();
 //app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server);
